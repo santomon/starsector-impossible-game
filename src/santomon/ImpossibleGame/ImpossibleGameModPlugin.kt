fabfc5b1b04cb69552c75444af44acfe8be34df4
santomon <simon.bohlender@kgu.de>
2024-08-06 15:41:04 +0200

    xdd

--- src/santomon/ImpossibleGame/ImpossibleGameModPlugin.kt
+++ src/santomon/ImpossibleGame/ImpossibleGameModPlugin.kt
@@ -2,11 +2,5 @@ package santomon.ImpossibleGame
 
 import com.fs.starfarer.api.BaseModPlugin
 
-
 class ImpossibleGameModPlugin: BaseModPlugin(){
-
-    override fun onApplicationLoad() {
-        super.onApplicationLoad()
-        println("ImpossibleGameModPlugin loaded")
-    }
 }
