5964a1751f8b74026b8153c20bd72e6782e2062c
santomon <simon.bohlender@kgu.de>
2024-08-06 15:28:52 +0200

    maybe?

---
+++ src/santomon/ImpossibleGame/ImpossibleGameModPlugin.kt
@@ -0,0 +1,12 @@
+package santomon.ImpossibleGame
+
+import com.fs.starfarer.api.BaseModPlugin
+
+
+class ImpossibleGameModPlugin: BaseModPlugin(){
+
+    override fun onApplicationLoad() {
+        super.onApplicationLoad()
+        println("ImpossibleGameModPlugin loaded")
+    }
+}
