ab9660b414b12b0d34ec8503e89753e5ded93827
santomon <qa12_8@yahoo.de>
2024-08-18 00:00:04 +0200

    hmm

--- data/missions/xddmission/MissionDefinition.java
+++ data/missions/xddmission/MissionDefinition.java
@@ -22,8 +22,10 @@ public class MissionDefinition implements MissionDefinitionPlugin {
 		// Set up the fleets so we can add ships and fighter wings to them.
 		// In this scenario, the fleets are attacking each other, but
 		// in other scenarios, a fleet may be defending or trying to escape
-		api.initFleet(FleetSide.PLAYER, "TTS", FleetGoal.ATTACK, false, 5);
-		api.initFleet(FleetSide.ENEMY, "HSS", FleetGoal.ATTACK, true);
+		api.initFleet(FleetSide.PLAYER, "xdd", FleetGoal.ATTACK, false, 5);
+		api.initFleet(FleetSide.ENEMY, "fumo", FleetGoal.ATTACK, true);
+
+
 
 		// Set a small blurb for each fleet that shows up on the mission detail and
 		// mission results screens to identify each side.
@@ -33,32 +35,15 @@ public class MissionDefinition implements MissionDefinitionPlugin {
 		// These show up as items in the bulleted list under 
 		// "Tactical Objectives" on the mission detail screen
 		api.addBriefingItem("Defeat all enemy forces");
-
-		// Set up the player's fleet.  Variant names come from the
-		// files in data/variants and data/variants/fighters
-		//api.addToFleet(FleetSide.PLAYER, "harbinger_Strike", FleetMemberType.SHIP, "TTS Invisible Hand", true, CrewXPLevel.VETERAN);
-		api.addToFleet(FleetSide.PLAYER, "doom_Strike", FleetMemberType.SHIP, "TTS Invisible Hand", true);
-		api.addToFleet(FleetSide.PLAYER, "afflictor_Strike", FleetMemberType.SHIP, "TTS Antithesis", false);
-		api.addToFleet(FleetSide.PLAYER, "shade_Assault", FleetMemberType.SHIP, "TTS Blind Consequence", false);
-
 		api.defeatOnShipLoss("TTS Invisible Hand");
 		
 		// Set up the enemy fleet.
 		api.addToFleet(FleetSide.ENEMY, "enforcer_Elite", FleetMemberType.SHIP, "HSS Judicature", true);
 
 		// Set up the map.
-		float width = 24000f;
-		float height = 18000f;
+		float width = 4000f;
+		float height = 4000f;
 		api.initMap((float)-width/2f, (float)width/2f, (float)-height/2f, (float)height/2f);
-		
-
-
-		for (int i = 0; i < 5; i++) {
-			float x = (float) Math.random() * width - width/2;
-			float y = (float) Math.random() * height - height/2;
-			float radius = 100f + (float) Math.random() * 400f; 
-			api.addNebula(x, y, radius);
-		}
 
 		ImpossibleGameLevelPlugin plugin = new ImpossibleGameLevelPlugin();
 		api.addPlugin(plugin);
