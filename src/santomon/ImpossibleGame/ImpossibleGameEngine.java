99b32b809dcd596e74270b4e4e21da0bc602c631
santomon <simon.bohlender@kgu.de>
2024-08-21 16:52:24 +0200

    adding the kite

--- src/santomon/ImpossibleGame/ImpossibleGameEngine.java
+++ src/santomon/ImpossibleGame/ImpossibleGameEngine.java
@@ -4,6 +4,9 @@ import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.combat.ShipAPI;
 import com.fs.starfarer.api.combat.listeners.AdvanceableListener;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ImpossibleGameEngine implements AdvanceableListener {
     // a lot of the logic is put here to enable pausing maybe?
 
@@ -12,6 +15,11 @@ public class ImpossibleGameEngine implements AdvanceableListener {
     public float currentSecondStack = 0f;  // 1f === 1 sec; advanced amount is in the ~0.017 range usually
     public final float spawnInterval = 1f;
     public final float objectVelocity = 10f;
+    public final HashMap<Integer, String> objectLookUpTable = new HashMap<Integer, String>() {{
+        put(0, null);
+        put(1, "");  // spikes
+    }
+    };
 
 
     public ImpossibleGameEngine(int[][] levelData, float mapSizeX, float mapSizeY) {
