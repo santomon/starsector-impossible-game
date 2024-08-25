313fec91584487b2dac96c1a5686ac6a8b8e8d94
santomon <simon.bohlender@kgu.de>
2024-08-25 21:22:09 +0200

    starting to implement the jump script as a separate component

---
+++ src/santomon/ImpossibleGame/JumpScript.java
@@ -0,0 +1,38 @@
+package santomon.ImpossibleGame;
+
+import com.fs.starfarer.api.combat.ShipAPI;
+import com.fs.starfarer.api.combat.listeners.AdvanceableListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JumpScript implements AdvanceableListener {
+
+    public List<String> groundShipIDs = new ArrayList<String>();
+    public ShipAPI jumper;
+    public JumpSettings settings;
+    private boolean gravityIsReversed = false;
+
+    public JumpScript(ShipAPI jumper, List<String> groundShipIDs, JumpSettings settings) {
+        this.groundShipIDs = groundShipIDs;
+        this.jumper = jumper;
+        this.settings = settings;
+    }
+
+
+    @Override
+    public void advance(float amount) {
+
+    }
+
+    public void maybeTriggerJump() {
+
+    }
+
+    public void setGravityisReversed(boolean reversed) {
+        this.gravityIsReversed = reversed;
+    }
+    public boolean getGravityIsReversed() {
+        return this.gravityIsReversed;
+    }
+}
