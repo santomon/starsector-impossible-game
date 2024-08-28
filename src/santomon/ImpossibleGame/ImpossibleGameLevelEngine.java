c5cf264a55d5f9032daa593e0521b67e5f73613b
santomon <simon.bohlender@kgu.de>
2024-08-28 14:34:59 +0200

    concept where we spawn ships from left to right to avoid potential async collisions maybe?

--- src/santomon/ImpossibleGame/ImpossibleGameLevelEngine.java
+++ src/santomon/ImpossibleGame/ImpossibleGameLevelEngine.java
@@ -12,6 +12,7 @@ import org.lwjgl.util.vector.Vector2f;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 
 
@@ -32,7 +33,7 @@ public class ImpossibleGameLevelEngine extends BaseEveryFrameCombatPlugin {
     public static final float spawnInterval = objectVelocity * 0.0002f;
     public static final float topPadding = 100f;
     public static final float rightPadding = 100f;
-    public static final float tileSize = 128f;  // kite has  a collision radius of 64
+    public static final float tileSize = 150f;  // kite has  a collision radius of 64
 
 
     public ImpossibleGameLevelEngine(int[][] levelData, float mapSizeX, float mapSizeY, final HashMap<Integer, String> objectLookUpTable) {
@@ -92,12 +93,17 @@ public class ImpossibleGameLevelEngine extends BaseEveryFrameCombatPlugin {
 
         Vector2f lowestPointSpawnPosition = calculateSpawnPosition(i, this.mapSizeX, this.mapSizeY);
         Vector2f currentSpawnPosition = new Vector2f().set(lowestPointSpawnPosition);
+        List<Float> xSpawnPositions = new LinkedList<Float>();
 
         while (currentSpawnPosition.x >= - mapSizeX / 2) {
-            ShipAPI groundShip = enemyFleetManagerAPI.spawnShipOrWing(objectLookUpTable.get(1), currentSpawnPosition, 90f);
+            xSpawnPositions.add(0, currentSpawnPosition.x);
+            currentSpawnPosition.setX(currentSpawnPosition.x - tileSize);
+        }
+
+        for (float xSpawnPosition : xSpawnPositions) {
+            ShipAPI groundShip = enemyFleetManagerAPI.spawnShipOrWing(objectLookUpTable.get(1), new Vector2f(xSpawnPosition, lowestPointSpawnPosition.y), 90f);
             groundShip.getVelocity().set(- objectVelocity, 0);
             groundShip.makeLookDisabled();
-            currentSpawnPosition.setX(currentSpawnPosition.x - tileSize);
         }
 
     }
