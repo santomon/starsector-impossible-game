2b8b9d84c782147edaea1adc5c4b412afea1f05f
santomon <simon.bohlender@kgu.de>
2024-09-17 19:17:22 +0200

    delayed flux. although we prob should have postponed the implementation of that class

--- src/santomon/ImpossibleGame/KanseiDrift.java
+++ src/santomon/ImpossibleGame/KanseiDrift.java
@@ -4,24 +4,40 @@ import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.combat.MutableShipStatsAPI;
 import com.fs.starfarer.api.combat.ShipAPI;
 import com.fs.starfarer.api.combat.ShipSystemAPI;
+import com.fs.starfarer.api.combat.listeners.AdvanceableListener;
 import com.fs.starfarer.api.impl.combat.BaseShipSystemScript;
 import org.apache.log4j.Logger;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class KanseiDrift extends BaseShipSystemScript {
     Logger log = Global.getLogger(KanseiDrift.class);
-    ShipAPI ship;
+
+    static final float targetInitialAngle = 45f;
+    static final float explosiveAcceleration = 900f;
 
 
     public void apply(MutableShipStatsAPI stats, String id, State state, float effectLevel) {
         ShipAPI ship = (ShipAPI) stats.getEntity();
+
+        if (!ship.hasListenerOfClass(DelayedFlux.class)) {
+            ship.addListener(new DelayedFlux(ship));
+        }
+
         log.info("effectLevel: " + effectLevel);
-        if (this.ship == null) return;  // shouldnt be possible, but just in case;
 
         if (state == State.IN) ;
 
 
 
     }
+
+    private void kanseiDriftPhase1() {
+
+    }
+
+
 
     public void unapply(MutableShipStatsAPI stats, String id) {
     }
@@ -32,8 +48,6 @@ public class KanseiDrift extends BaseShipSystemScript {
 
 
     public boolean isUsable(ShipSystemAPI system, ShipAPI ship) {
-        // gets called like, every frame
-        if (this.ship == null) this.ship = ship;
         return true;
     }
 
@@ -55,6 +69,54 @@ public class KanseiDrift extends BaseShipSystemScript {
 
     public int getUsesOverride(ShipAPI ship) {
         return -1;
+    }
+
+
+}
+
+
+class DelayedFlux implements AdvanceableListener {
+
+    final ShipAPI ship;
+    final List<Float> fluxToApply = new ArrayList<Float>();
+
+    final float fluxRate = 400; // flux/s
+    final boolean hardFlux = true;
+
+    DelayedFlux (ShipAPI ship) {
+        this.ship = ship;
+    }
+
+    public void addFlux(float amount) {
+        this.fluxToApply.add(amount);
+    }
+
+    @Override
+    public void advance(float amount) {
+        if (ship == null) return;
+
+        applyFlux(amount);
+        pruneFluxToApply();
+    }
+
+    private void applyFlux(float timePassed) {
+
+        float targetAmount = fluxRate * timePassed;
+        float stack = 0;
+        for (int i = 0; i < fluxToApply.size(); i++) {
+            float amount = Math.min(fluxToApply.get(i), targetAmount);
+            amount = Math.max(0, amount);
+            stack += amount;
+            fluxToApply.set(i, fluxToApply.get(i) - amount);
+        }
+        ship.getFluxTracker().increaseFlux(stack, hardFlux);
+    }
+
+    private void pruneFluxToApply() {
+        for (int i = fluxToApply.size() - 1; i >= 0 ; i--) {
+            if (fluxToApply.get(i) <= 0) fluxToApply.remove(i);
+        }
+
     }
 
 
