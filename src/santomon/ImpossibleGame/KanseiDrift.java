ad8008b7375c543f4e69f2de0c36547f21563974
santomon <simon.bohlender@kgu.de>
2024-09-17 19:53:40 +0200

    ok we have the delayed flux cost. but tbh, dont think our implementation makes much sense. prob should simply be using the csv interface for fluxcost instead

--- src/santomon/ImpossibleGame/KanseiDrift.java
+++ src/santomon/ImpossibleGame/KanseiDrift.java
@@ -1,33 +1,68 @@
 package santomon.ImpossibleGame;
 
 import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.combat.CombatEngineAPI;
 import com.fs.starfarer.api.combat.MutableShipStatsAPI;
 import com.fs.starfarer.api.combat.ShipAPI;
 import com.fs.starfarer.api.combat.ShipSystemAPI;
 import com.fs.starfarer.api.combat.listeners.AdvanceableListener;
 import com.fs.starfarer.api.impl.combat.BaseShipSystemScript;
 import org.apache.log4j.Logger;
+import santomon.ImpossibleGame.hullmods.PortAssault;
+import santomon.ImpossibleGame.hullmods.StarboardAssault;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class KanseiDrift extends BaseShipSystemScript {
     Logger log = Global.getLogger(KanseiDrift.class);
+    boolean hasBeenActivated = false;
 
     static final float targetInitialAngle = 45f;
     static final float explosiveAcceleration = 900f;
+    static final float fluxCost = 5000;
 
 
     public void apply(MutableShipStatsAPI stats, String id, State state, float effectLevel) {
+
+
         ShipAPI ship = (ShipAPI) stats.getEntity();
+        CombatEngineAPI combatEngineAPI = Global.getCombatEngine();
 
         if (!ship.hasListenerOfClass(DelayedFlux.class)) {
             ship.addListener(new DelayedFlux(ship));
         }
+        if (!this.hasBeenActivated) {
+            this.hasBeenActivated = true;
+            List<DelayedFlux> delayedFluxes = ship.getListeners(DelayedFlux.class);
+            if (delayedFluxes.isEmpty()) return;  // impossible, right???
+            delayedFluxes.get(0).addFlux(fluxCost);
+        }
 
         log.info("effectLevel: " + effectLevel);
 
-        if (state == State.IN) ;
+        if (state == State.IN) {
+            boolean right = ship.getVariant().getHullMods().contains(StarboardAssault.tag);
+            if (!right) {
+                if (!ship.getVariant().getHullMods().contains(PortAssault.tag) ) {
+                    return;
+                }
+            }
+
+            float timePassed = combatEngineAPI.getElapsedInLastFrame();
+
+        };
+
+        if (state == State.ACTIVE) {
+            // actual drifting
+
+        }
+
+
+        if (state == State.OUT) {
+            // state of extreme deceleration
+        }
+
 
 
 
@@ -35,11 +70,19 @@ public class KanseiDrift extends BaseShipSystemScript {
 
     private void kanseiDriftPhase1() {
 
+
+    }
+
+
+    private void kanseiDriftPhase2() {
+
     }
 
 
 
     public void unapply(MutableShipStatsAPI stats, String id) {
+        // called once at the end ig; maybe even once at combat start or sth..
+        this.hasBeenActivated = false;
     }
 
     public StatusData getStatusData(int index, State state, float effectLevel) {
@@ -101,7 +144,8 @@ class DelayedFlux implements AdvanceableListener {
 
     private void applyFlux(float timePassed) {
 
-        float targetAmount = fluxRate * timePassed;
+        float ventingMultiplier = this.ship.getFluxTracker().isVenting() ? this.ship.getMutableStats().getVentRateMult().getModifiedValue() : 1;
+        float targetAmount = ventingMultiplier * timePassed * fluxRate;
         float stack = 0;
         for (int i = 0; i < fluxToApply.size(); i++) {
             float amount = Math.min(fluxToApply.get(i), targetAmount);
