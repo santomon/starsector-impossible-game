d7918b621513e327b36887c820f7d085a33111f1
santomon <simon.bohlender@kgu.de>
2024-09-23 17:51:24 +0200

    hmm base logic for after images... i mean we really are stretching at this point...

--- src/santomon/ImpossibleGame/PhaseEcho.java
+++ src/santomon/ImpossibleGame/PhaseEcho.java
@@ -3,23 +3,35 @@ package santomon.ImpossibleGame;
 
 import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.combat.*;
+import com.fs.starfarer.api.combat.listeners.AdvanceableListener;
 import com.fs.starfarer.api.fleet.FleetMemberAPI;
 import com.fs.starfarer.api.fleet.FleetMemberType;
+import com.fs.starfarer.api.graphics.SpriteAPI;
 import com.fs.starfarer.api.impl.combat.BaseShipSystemScript;
+import org.apache.log4j.Logger;
+import org.lwjgl.util.vector.Vector2f;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PhaseEcho  extends BaseShipSystemScript {
 
 
     private ShipAPI anchorMarker;
+    private AfterImageEffect afterImageEffect;
 
     private String id;
     private static final float MANEUVERABILITY_BOOST = 0.5f;
     private static final float TOP_SPEED_BOOST = 80F;  // 30 more than
 
+    private static final float AFTER_IMAGE_DURATION = 0.5f;
+    private static final float AFTER_IMAGE_INTERVAL = 0.2f;
+
 
     public void apply(MutableShipStatsAPI stats, String id, State state, float effectLevel) {
 
         ShipAPI ship = (ShipAPI) stats.getEntity();
+        if (ship == null) return;
         this.id = id;
 
         if (!hasSpawnedAnchorMarker()) {
@@ -40,6 +52,8 @@ public class PhaseEcho  extends BaseShipSystemScript {
 
 
         if (state == State.OUT) {
+            if (!hasCreatedAfterImageEffect()) createAfterImageEffect(ship);
+            this.afterImageEffect.setActive(true);
             // copied from maneuvering jets
             stats.getMaxSpeed().unmodify(id); // to slow down ship to its regular top speed while powering drive down
             stats.getMaxTurnRate().unmodify(id);
@@ -62,6 +76,20 @@ public class PhaseEcho  extends BaseShipSystemScript {
 
 
 
+    }
+
+    private void createAfterImageEffect(ShipAPI ship) {
+        ShipSystemAPI shipSystemAPI = ship.getSystem();
+        this.afterImageEffect = new AfterImageEffect(
+                ship,
+                AFTER_IMAGE_DURATION,
+                AFTER_IMAGE_INTERVAL
+        );
+
+
+        if (!ship.hasListenerOfClass(AfterImageEffect.class)) {
+            ship.addListener(this.afterImageEffect);
+        }
     }
 
     private void createAnchorMarker(ShipAPI ship) {
@@ -100,6 +128,10 @@ public class PhaseEcho  extends BaseShipSystemScript {
 
     private boolean hasSpawnedAnchorMarker() {
         return this.anchorMarker != null;
+    }
+
+    private boolean hasCreatedAfterImageEffect() {
+        return this.afterImageEffect != null;
 
     }
 
@@ -112,6 +144,10 @@ public class PhaseEcho  extends BaseShipSystemScript {
         if (ship == null) return;
 
         ship.setPhased(false);
+
+        if (this.afterImageEffect != null) {
+            this.afterImageEffect.setActive(false);
+        }
 
         stats.getMaxSpeed().unmodify(id);
         stats.getMaxTurnRate().unmodify(id);
@@ -129,3 +165,121 @@ public class PhaseEcho  extends BaseShipSystemScript {
 
 
 }
+
+
+class AfterImageEffect implements AdvanceableListener {
+    private final ShipAPI ship;
+    private final Logger log = Global.getLogger(AfterImageEffect.class);
+
+    private float timeSinceLastAfterImageSpawned = 0;
+    private float timeElapsedSinceInstantiation = 0;
+
+    private float afterImageDuration = 0.5f;
+    private float afterImageInterval = 0.5f;
+    private boolean active = false;
+
+    private List<TimestampedData> timestampedDataList = new ArrayList<>();
+    private List<TimestampedData> toRender = new ArrayList<>();
+
+    AfterImageEffect(ShipAPI ship, Float afterImageDuration, Float afterImageInterval) {
+        this.ship = ship;
+        if (afterImageDuration != null) {
+            this.afterImageDuration = afterImageDuration;
+        }
+        if (afterImageInterval != null) {
+            this.afterImageInterval = afterImageInterval;
+        }
+
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    @Override
+    public void advance(float amount) {
+        // not sure how this after image thing behaves with time acceleration?
+        timeElapsedSinceInstantiation += amount;
+        timeSinceLastAfterImageSpawned += amount;
+        if (!this.active) return;
+        if (this.ship == null) return;
+
+        timestampedDataList.add(new TimestampedData(timeElapsedSinceInstantiation, new Vector2f().set(ship.getLocation()), ship.getFacing(), afterImageDuration));
+
+
+
+        if (timeSinceLastAfterImageSpawned > afterImageInterval) {
+            timeSinceLastAfterImageSpawned -= afterImageInterval;
+            addToRender();
+            pruneTimestampedData();
+        }
+
+        maybeRender(amount);
+        pruneToRender();
+    }
+
+    private void pruneToRender() {
+        List<TimestampedData> prunedToRender = new ArrayList<>();
+        for (TimestampedData toRenderElement : this.toRender) {
+            if (toRenderElement.remainingLifeTime > 0) {
+                prunedToRender.add(toRenderElement);
+            }
+        }
+        this.toRender = prunedToRender;
+    }
+
+
+    private void addToRender() {
+        for (TimestampedData timestampedDataElement: this.timestampedDataList) {
+            if (this.timeElapsedSinceInstantiation - timestampedDataElement.timestamp < afterImageInterval) {
+                this.toRender.add(timestampedDataElement);
+                break;
+            }
+        }
+    }
+
+    private void pruneTimestampedData() {
+        List<TimestampedData> prunedData = new ArrayList<>();
+        for (TimestampedData timestampedDataElement : this.timestampedDataList) {
+            if (this.timeElapsedSinceInstantiation - timestampedDataElement.timestamp < afterImageInterval) {
+                prunedData.add(timestampedDataElement);
+            }
+        }
+        this.timestampedDataList = prunedData;
+
+    }
+
+    private void maybeRender(float amount) {
+
+        for (TimestampedData timestampedData : this.toRender) {
+            log.info("are we even rendering shit?");
+
+            timestampedData.remainingLifeTime -= amount;
+            String spriteName = ship.getHullSpec().getSpriteName();
+            SpriteAPI spriteAPI = Global.getSettings().getSprite(spriteName);  // hopefully this creates a fresh object
+            spriteAPI.setAngle(timestampedData.facing - 90f);
+            spriteAPI.renderAtCenter(timestampedData.location.x, timestampedData.location.y);
+        }
+
+    }
+}
+
+
+class TimestampedData {
+
+        public final Vector2f location;
+        public final float facing;
+        public final float timestamp;
+        public float remainingLifeTime;
+
+        TimestampedData(float timestamp, Vector2f location, float facing, float lifeTime) {
+            this.facing = facing;
+            this.timestamp = timestamp;
+            this.location = location;
+            this.remainingLifeTime = lifeTime;
+        }
+        }
