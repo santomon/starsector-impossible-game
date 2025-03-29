2fb5848f0e20bf2a0a21e9b681235081dce40075
santomon <simon.bohlender@kgu.de>
2025-03-29 17:21:13 +0100

    can we even play music like this?

--- src/santomon/ImpossibleGame/ImpossibleGameLevelPlugin.java
+++ src/santomon/ImpossibleGame/ImpossibleGameLevelPlugin.java
@@ -8,79 +8,37 @@ import com.fs.starfarer.api.combat.ShipAPI;
 import com.fs.starfarer.api.input.InputEventAPI;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+
 import java.awt.Color;
 
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.util.vector.Vector2f;
 
+
 import java.util.*;
 import java.util.List;
 
-public class ImpossibleGameLevelPlugin extends BaseEveryFrameCombatPlugin {
 
-    public final int[][] levelData;
-    public final int[] gravityData;
-    private Vector2f safeSpot;
-    public final HashMap<Integer, Color> colorData;
-    public final JumpSettings jumpSettings = new JumpSettings(
-            4000,
-            9111.2f,
-            100000,
-            30
-            );
-    public final KeyBindings keyBindings = new KeyBindings(new ArrayList<>() {{
-        add(Keyboard.KEY_E);
-    }});
-    public final String jumperVariantID = IGMisc.IDs.IG_HERMES_VARIANT_ID;
+class ImpossibleMusicHandler extends BaseEveryFrameCombatPlugin {
 
-    public ImpossibleGameLevelEngine impossibleGameLevelEngine;
-    public ShipAPI jumper;
-    public JumpScript jumpScript;
-    public KillPlayerWhenAnyPlayerDamageIsTaken killPlayerWhenAnyPlayerDamageIsTakenScript;
+    final String soundTrackId;
 
-
-    public static final HashMap<Integer, String> objectLookUpTable = new HashMap<Integer, String>() {{
-        put(0, null);
-        put(1, IGMisc.IDs.IG_WARDEN_ID);  // block
-        put(2, IGMisc.IDs.IG_KITE_VARIANT_ID);  // spikes
-        put(3, "radiant_Standard");  // victory
-        put(9, null);  // some other null markers
+    ImpossibleMusicHandler(String levelName) {
+        this.soundTrackId = "impossible_" + levelName + "_ost";
     }
-    };
-    public static final List<String> groundShipIDs = new ArrayList<String>() {{
-        add(objectLookUpTable.get(1));
-    }};
-    private CameraControl cameraControl;
-    public Boolean hasCalledFakeInit = false;
-
-
-    public ImpossibleGameLevelPlugin(String levelName) {
-
-        System.out.println("ImpossibleGameLevelPlugin constructor");
-        this.levelData = loadLevelData(levelName);
-        this.gravityData = loadGravityData(levelName);
-        this.colorData = loadColorData(levelName);
-    }
-
 
     @Override
-    public void advance(float amount, List<InputEventAPI> events) {
-        if (!hasCalledFakeInit){
-            fakeInit(Global.getCombatEngine());
-            hasCalledFakeInit = true;
-        }
+    public void init(CombatEngineAPI engine) {
+        play();
+    }
 
+    public void restart() {
+        play();
+    }
 
-        // maybe quickrestart
-        if (this.quickRestartPressed(events)) {
-            this.cleanUp();
-        }
-
-        maybeStopMusic();
-
-
-        lockFlagShips();
-     }
+    private void play() {
+        Global.getSoundPlayer().playCustomMusic(1, 1, this.soundTrackId);
+    }
 
 
     private void maybeStopMusic() {
@@ -93,6 +51,79 @@ public class ImpossibleGameLevelPlugin extends BaseEveryFrameCombatPlugin {
 //            Global.getSoundPlayer().pauseCustomMusic();
 //        }
     }
+}
+
+public class ImpossibleGameLevelPlugin extends BaseEveryFrameCombatPlugin {
+
+    public final int[][] levelData;
+    public final int[] gravityData;
+    private Vector2f safeSpot;
+    public final HashMap<Integer, Color> colorData;
+    public final JumpSettings jumpSettings = new JumpSettings(
+            4000,
+            9111.2f,
+            100000,
+            30
+    );
+    public final KeyBindings keyBindings = new KeyBindings(new ArrayList<>() {{
+        add(Keyboard.KEY_E);
+    }});
+    public final String jumperVariantID = IGMisc.IDs.IG_HERMES_VARIANT_ID;
+
+    public ImpossibleGameLevelEngine impossibleGameLevelEngine;
+    public ShipAPI jumper;
+    public JumpScript jumpScript;
+    public KillPlayerWhenAnyPlayerDamageIsTaken killPlayerWhenAnyPlayerDamageIsTakenScript;
+
+
+    public static final HashMap<Integer, String> objectLookUpTable = new HashMap<Integer, String>() {
+        {
+            put(0, null);
+            put(1, IGMisc.IDs.IG_WARDEN_ID);  // block
+            put(2, IGMisc.IDs.IG_KITE_VARIANT_ID);  // spikes
+            put(3, "radiant_Standard");  // victory
+            put(9, null);  // some other null markers
+        }
+    };
+    public static final List<String> groundShipIDs = new ArrayList<String>() {{
+        add(objectLookUpTable.get(1));
+    }};
+    private CameraControl cameraControl;
+    ImpossibleMusicHandler impossibleMusicHandler;
+    public Boolean hasCalledFakeInit = false;
+
+
+    public ImpossibleGameLevelPlugin(String levelName) {
+
+        System.out.println("ImpossibleGameLevelPlugin constructor");
+        this.levelData = loadLevelData(levelName);
+        this.gravityData = loadGravityData(levelName);
+        this.colorData = loadColorData(levelName);
+
+
+        this.impossibleMusicHandler = new ImpossibleMusicHandler(levelName);
+        Global.getCombatEngine().addPlugin(this.impossibleMusicHandler);
+
+    }
+
+
+    @Override
+    public void advance(float amount, List<InputEventAPI> events) {
+        if (!hasCalledFakeInit) {
+            fakeInit(Global.getCombatEngine());
+            hasCalledFakeInit = true;
+        }
+
+
+        // maybe quickrestart
+        if (this.quickRestartPressed(events)) {
+            this.cleanUp();
+        }
+
+        lockFlagShips();
+    }
+
+
 
     @Override
     public void init(CombatEngineAPI engine) {
@@ -109,7 +140,6 @@ public class ImpossibleGameLevelPlugin extends BaseEveryFrameCombatPlugin {
                 engine.getMapHeight() / 2 - 500
         );
 
-        Global.getSoundPlayer().playCustomMusic(1, 1, IGMisc.IDs.IG_CHAOZ_FANTASY_OST);
         SoundPlayerAPI soundPlayer = Global.getSoundPlayer();
         engine.addPlugin(this.impossibleGameLevelEngine);
 
@@ -140,31 +170,31 @@ public class ImpossibleGameLevelPlugin extends BaseEveryFrameCombatPlugin {
 
     }
 
-   public void createJumper() {
-       CombatEngineAPI engine = Global.getCombatEngine();
-       this.jumper = engine.getFleetManager(0).spawnShipOrWing(jumperVariantID, new Vector2f(0, 0), 0);  // facing of 0 === looking to the right
-       this.jumper.getVelocity().set(0, 0);
-       this.jumper.setShipAI(new DontMoveAI());
-       this.jumper.addListener(new DamageWhenOutOfBounds(this.jumper));
+    public void createJumper() {
+        CombatEngineAPI engine = Global.getCombatEngine();
+        this.jumper = engine.getFleetManager(0).spawnShipOrWing(jumperVariantID, new Vector2f(0, 0), 0);  // facing of 0 === looking to the right
+        this.jumper.getVelocity().set(0, 0);
+        this.jumper.setShipAI(new DontMoveAI());
+        this.jumper.addListener(new DamageWhenOutOfBounds(this.jumper));
 
-       this.jumpScript = new JumpScript(this.jumper, groundShipIDs, this.jumpSettings);
-       this.impossibleGameLevelEngine.getJumpScripts().add(this.jumpScript);
-       engine.addPlugin(this.jumpScript);
-       if (this.impossibleGameLevelEngine != null) {
-           this.impossibleGameLevelEngine.positionJumper(this.jumper);
-       }
+        this.jumpScript = new JumpScript(this.jumper, groundShipIDs, this.jumpSettings);
+        this.impossibleGameLevelEngine.getJumpScripts().add(this.jumpScript);
+        engine.addPlugin(this.jumpScript);
+        if (this.impossibleGameLevelEngine != null) {
+            this.impossibleGameLevelEngine.positionJumper(this.jumper);
+        }
 
-       this.cameraControl = new CameraControl(this.jumper);
-       Global.getCombatEngine().addPlugin(this.cameraControl);
-       cameraControl.setIsActive(true);
+        // probably shouldnt be doing it like this...
+        this.cameraControl = new CameraControl(this.jumper);
+        Global.getCombatEngine().addPlugin(this.cameraControl);
+        cameraControl.setIsActive(true);
 
-   }
+    }
 
 
-
-    public static int[][] loadLevelData(String levelName){
-        try{
-            String levelDataRaw = Global.getSettings().loadText("data/missions/"+levelName+"/level_data.txt");
+    public static int[][] loadLevelData(String levelName) {
+        try {
+            String levelDataRaw = Global.getSettings().loadText("data/missions/" + levelName + "/level_data.txt");
             String[] Q = levelDataRaw.split("\n");
             String firstRow = Q[0];
             int[][] data = new int[Q.length][firstRow.length()];
@@ -177,30 +207,30 @@ public class ImpossibleGameLevelPlugin extends BaseEveryFrameCombatPlugin {
             }
             int[][] result = IGMisc.transposeMatrix(data);
             return result;
-        } catch (Exception e){
+        } catch (Exception e) {
             throw new RuntimeException("Failed to load level data", e);
         }
     }
 
-    public static int[] loadGravityData(String levelName){
+    public static int[] loadGravityData(String levelName) {
         try {
-            String gravityDataRaw = Global.getSettings().loadText("data/missions/"+levelName+"/gravity_data.txt");
+            String gravityDataRaw = Global.getSettings().loadText("data/missions/" + levelName + "/gravity_data.txt");
             String[] Q = gravityDataRaw.split("\n");
             int[] data = new int[Q.length];
             for (int i = 0; i < Q.length; i++) {
                 data[i] = Integer.parseInt(Q[i]);
             }
             return data;
-        } catch (Exception e ) {
+        } catch (Exception e) {
             getLogger().error("Failed to load gravity data. make sure, it is exactly 1 integer per line and nothing else.", e);
             return new int[0];
         }
     }
 
-    public static HashMap<Integer, Color> loadColorData(String levelName){
+    public static HashMap<Integer, Color> loadColorData(String levelName) {
         try {
             HashMap<Integer, Color> data = new HashMap<>();
-            String colorDataRaw = Global.getSettings().loadText("data/missions/"+levelName+"/color_data.txt");
+            String colorDataRaw = Global.getSettings().loadText("data/missions/" + levelName + "/color_data.txt");
             String[] Q = colorDataRaw.split("\n");
             for (int i = 0; i < Q.length; i++) {
                 int[] tmp = new int[4];
@@ -218,7 +248,7 @@ public class ImpossibleGameLevelPlugin extends BaseEveryFrameCombatPlugin {
         }
     }
 
-    public static void showLevelData(int[][] levelData){
+    public static void showLevelData(int[][] levelData) {
 
         for (int[] row : levelData) {
             for (int i : row) {
